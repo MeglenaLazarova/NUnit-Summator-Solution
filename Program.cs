8fe9882208de739511f50b129fb4fe7df20675c4
Меглена Лазарова <39823149+MeglenaLazarova@users.noreply.github.com>
2022-05-08 08:22:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Summator
+{
+    public class Program
+    {
+        static void Main (string[] args)
+        {
+            long result = Summator.Sum(new long[] { 1, 2, 3, 4, 5, 6 });
+
+            if (result == 20)
+            {
+                Console.WriteLine("TEST PASS");
+            }
+            else
+            {
+                Console.WriteLine("TEST FAIL");
+            }
+
+        }
+    }
+}
+
+
